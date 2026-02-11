9157886b0f7b53b77d7e603d73f79b8bc8f01454
mac <mac@macs-MacBook-Pro.local>
2026-02-11 11:26:27 -0600

    Fix build: update internal paths to use underscores

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X6517
+DEVICE_PATH := device/infinix/Infinix_X6517
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
--- omni_Infinix_X6517.mk
+++ omni_Infinix_X6517.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Infinix-X6517 device
-$(call inherit-product, device/infinix/Infinix-X6517/device.mk)
+$(call inherit-product, device/infinix/Infinix_X6517/device.mk)
 
 PRODUCT_DEVICE := Infinix_X6517
 PRODUCT_NAME := omni_Infinix_X6517
